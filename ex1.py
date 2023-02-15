ac1fa31f111845dd0c15fbcf28e7ef202b9b8a70
anass <anass.bekkali@uae.ac.ma>
2023-02-15 15:23:26 +0100

    exo 1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+
+for i in range(1, 101):
+    print(i)
+
+    if i % 3 == 0:
+        print("Hello")
+
+    elif i % 5 == 0:
+        print("World")
+        
+    elif i % 7 == 0:
+        print("Yoo")
+
+        
